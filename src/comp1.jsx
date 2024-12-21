680e2722e0a8f0bf16f5ad6bdad4ac76d27da444
jsvirani <jaydeepvirani677@gmail.com>
2024-12-21 18:14:03 +0530

    Initial commit

---
+++ src/comp1.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+class MyComponent extends React.Component{
+    render(){
+        return (
+        <div>   
+            Hello World  
+            <h1>React H1</h1>
+            <h2>React H2</h2>
+            <h3>React H3</h3>
+        </div>
+        );
+    }
+}
+export default MyComponent;
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import MyComponent  from "./comp1";
+
+createRoot(document.getElementById('root')).render(
+  <div>
+    <MyComponent />
+  </div>
+);
