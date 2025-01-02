4c830b677008f191042d41771a8797a43f1369db
jsvirani <jaydeepvirani677@gmail.com>
2025-01-02 11:18:33 +0530

    Update stateexmp.jsx

--- src/lec0201/stateexmp.jsx
+++ src/lec0201/stateexmp.jsx
@@ -1,11 +1,39 @@
 import { useState } from "react";
 function Stateexmps(){
-    const [name] = useState("Rku");
-    const [age] = useState("45");
+    const [name, setName] = useState("Jaydeep");
+    const [age, setAge] = useState("21");
+    const [color, setColor] = useState("red");
+    const [isChecked, setIsChecked] = useState(false);
     return (
         <div>
-            <h1>{name}</h1>
-            <h1>{age}</h1>
+            <input type="text" 
+                value={name} 
+                onChange={
+                            (e)=> setName(e.target.value)
+                        }
+            />
+            <p>Name of candidate is : {name}</p>
+            <br/>
+            <input type="number" 
+                value={age} 
+                onChange={
+                            (e)=> setAge(e.target.value)
+                        }
+            />
+            <p style={{background: color, height:50}}>
+                Age of candidate is : {age}
+            </p>
+            <input type="radio" name="C1"  onChange={()=> setColor("Blue")}/>
+            <input type="radio" name="C1"  onChange={()=> setColor("Yellow")}/>
+            <input type="radio" name="C1"  onChange={()=> setColor("Green")}/>
+
+            {color}
+            <br/>
+            <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={() => setIsChecked(!isChecked)} />
+            <p style={{fontWeight: isChecked ? "bold" : "normal"}} >Virani Jaydeep</p>
         </div>
     );
 }
